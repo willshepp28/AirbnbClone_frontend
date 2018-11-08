6c89db9b2d0250c001d7c554aa949259798a876b
willshepp28 <willsheppard28@outlook.com>
2018-11-08 15:48:05 -0500

    initial setup of login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../core/authentication/auth.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required]]
+    });
+  }
+
+
+
+  // convenience getter for easy access to form fields
+  get getFormControls() { return this.loginForm.controls; }
+
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+    console.log(this.loginForm.value);
+    // this.authService.login(this.loginForm.value)
+    //   .subscribe(
+    //     response => {
+    //       // set token, then redirect user to home page
+    //       localStorage.setItem("token", response.token);
+    //       this.router.navigate(["/home"]);
+    //     },
+    //     error => {
+    //       console.log(error);
+    //     });
   }
 
 }
