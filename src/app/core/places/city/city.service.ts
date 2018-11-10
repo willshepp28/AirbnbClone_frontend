8ce64f689fd9476bbc3c4e6e1523a0d4594e3bf0
willshepp28 <willsheppard28@outlook.com>
2018-11-10 16:33:52 -0500

    made a city service

---
+++ src/app/core/places/city/city.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CityService {
+
+  private url = "http://localhost:3000/api/v1/cities/";
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+  getRandomCities() {
+    return this.http.get<any>(this.url + "random");
+  }
+}
