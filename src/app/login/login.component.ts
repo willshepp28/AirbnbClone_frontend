516f5e84537806f5a41f2100d3b05d2b46ce6841
willshepp28 <willsheppard28@outlook.com>
2018-11-22 16:06:39 -0500

    created and display validator errors in login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -22,7 +22,7 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
       email: ["", [Validators.required, Validators.email]],
-      password: ["", [Validators.required]]
+      password: ["", [Validators.required, Validators.minLength(3)]]
     });
   }
 
